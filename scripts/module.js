4e9cec1d44985e26c85397e9202945228fc991a8
BrotherSharper <besardida@gmail.com>
2022-01-15 13:55:59 +0900

    add handling for items and journals

--- scripts/module.js
+++ scripts/module.js
@@ -1,24 +1,54 @@
 Hooks.on("renderActorSheet", (app, html, data) => {
-    if (game.settings.get("rm-header-text", "rmHeaderIconStyle") === true  ) {
-      html[0].classList.add("rmh");
-    }
+  if (game.settings.get("rm-header-text", "rmHeaderIconActorStyle") === true  ) {
+    html[0].classList.add("rmh");
+  }
+});
+
+Hooks.on("renderItemSheet", (app, html, data) => {
+  if (game.settings.get("rm-header-text", "rmHeaderIconItemStyle") === true  ) {
+    html[0].classList.add("rmh");
+  }
+});
+
+Hooks.on("renderJournalSheet", (app, html, data) => {
+  if (game.settings.get("rm-header-text", "rmHeaderIconJournalStyle") === true  ) {
+    html[0].classList.add("rmh");
+  }
+});
+
+Hooks.on("init", () => {
+//   game.settings.register("remove-header-icons", {
+//     scope: "world",
+//     config: false,
+//     type: Boolean,
+//     default: false
+//   });
+
+
+  game.settings.register("rm-header-text", "rmHeaderIconActorStyle", {
+    name: game.i18n.localize("rmHeaderText.actor.name"),
+    hint: game.i18n.localize("rmHeaderText.actor.hint"),
+    scope: "local",
+    config: true,
+    type: Boolean,
+    default: true       // デフォで使う
   });
-  
-  Hooks.on("init", () => {
-  //   game.settings.register("remove-header-icons", {
-  //     scope: "world",
-  //     config: false,
-  //     type: Boolean,
-  //     default: false
-  //   });
-  
-  
-    game.settings.register("rm-header-text", "rmHeaderIconStyle", {
-      name: game.i18n.localize("rmHeaderText.name"),
-      hint: game.i18n.localize("rmHeaderText.hint"),
-      scope: "local",
-      config: true,
-      type: Boolean,
-      default: false       // デフォでは使わない
-    });
+
+  game.settings.register("rm-header-text", "rmHeaderIconItemStyle", {
+    name: game.i18n.localize("rmHeaderText.item.name"),
+    hint: game.i18n.localize("rmHeaderText.item.hint"),
+    scope: "local",
+    config: true,
+    type: Boolean,
+    default: true       // デフォで使う
   });
+
+  game.settings.register("rm-header-text", "rmHeaderIconJournalStyle", {
+    name: game.i18n.localize("rmHeaderText.journal.name"),
+    hint: game.i18n.localize("rmHeaderText.journal.hint"),
+    scope: "local",
+    config: true,
+    type: Boolean,
+    default: true       // デフォで使う
+  });
+});
